fed6c51807f0c461c6483ed7fb05aed17f056eb2
Ronaldo Silva <60142058+ronaldosilva19@users.noreply.github.com>
2022-10-03 16:07:38 -0300

    Update function.cpp

--- minmax/src/function.cpp
+++ minmax/src/function.cpp
@@ -11,9 +11,54 @@
  */
 
 std::pair<int,int> min_max( int V[], size_t n )
-{
-    // TODO: Adicione aqui sua solução.
+{int maior = 0; // Menor elemento do vetor.
+    int menor = 0; // Maior elemento do vetor.
+    int posicao_menor; // Primeira posição do menor elemento do vetor. 
+    int posicao_maior; // Ultima posição do maior elemento do vetor. 
 
-    // TODO: Isso é apenas um STUB. Substitua com seu retorno correto.
-    return { -1, -1 };
+    pair<int, int> par; // Cria um objeto do tipo pair.
+
+    if(n == 0){
+         return { -1, -1 };
+    }else{
+      // Laço que procura o menor e maior elemento do vetor.
+      for(int i = 0; i < n; i++){
+              if(i == 0){
+                  maior = V[i];
+                  menor = V[i];  
+              }else{
+                  if(V[i] > maior){
+                      maior = V[i];
+                  }
+                  
+                  if(V[i] < menor){
+                      menor = V[i];
+                }
+            }
+        }
+    }
+
+  // Laço que procura a primeira ocorrencia do menor elemento do vetor.
+    for(int j = 0; j < n; j++){
+      if(V[j] == menor){
+        // std::cout << "Posicao menor: " << j;
+        posicao_menor = j;
+        break; // sai do laço, porque encontrou.
+      }
+    }
+      
+    // Laço que procura a ultima ocorrencia do maior elemento do vetor.
+    for(int k = 0; k < n; k++){
+        if(V[k] == maior){
+            posicao_maior = k;
+            k = posicao_maior;
+            
+        }
+    }
+
+  // Adiciona as respectivas posições no objeto par.
+    par.first = posicao_menor;
+    par.second = posicao_maior;
+
+   return par; // retorna as posições.
 }
