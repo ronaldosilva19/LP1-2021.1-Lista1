2522fc0cfa61357de86c91ac58c9b0716a1e8a12
Ronaldo Silva <60142058+ronaldosilva19@users.noreply.github.com>
2022-10-03 16:09:52 -0300

    Update function.cpp

--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -8,6 +8,48 @@ location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
 {
     // TODO: Coloque aqui seu código.
 
-    // TODO: Substitua o retorno conforme desejar. Isso é apenas um STUB, usado apenas para compilar.
-    return location_t::OUTSIDE;
+     Ponto inferior = {};
+    Ponto superior = {};
+
+  // Vefificar se os pontos formam um retanulo.
+    if ((IE.x != SD.x && IE.y != SD.y) || IE.x + SD.x != IE.y + SD.y){
+        if(IE.y < SD.y){
+          inferior.x = IE.x;
+          inferior.y = IE.y;
+          superior.x = SD.x;
+          superior.y = SD.y;
+        }else if(IE.y > SD.y){
+          inferior.x = SD.x;
+          inferior.y = SD.y;
+          superior.x = IE.x;
+          superior.y = IE.y;
+        }
+
+  // Retorna Dentro.
+    if ((P.x > inferior.x || P.x < superior.y) && P.y > inferior.y) {
+        if ((P.x < superior.x || P.x > superior.x) && P.y < superior.y) {
+            return location_t::INSIDE; // retorna inside.
+        }
+       }
+
+  // Retorna na Borda.
+      
+    if(P.x >= inferior.x && P.x <= superior.x && P.y == inferior.y){
+        if(P.x >= inferior.x && P.x <= superior.x && P.y == superior.y){ 
+            if(P.y >= inferior.y && P.y <= superior.y && P.x == inferior.x){
+              if(P.y >= inferior.y && P.y <= superior.y && P.x == superior.y){
+            return location_t::BORDER;  
+            }
+          } 
+        }else if((P.x == inferior.x && P.y == inferior.y) || (P.x == superior.x && P.y == superior.y)){
+            return location_t::BORDER; 
+        }else if((P.x == inferior.x && P.y == superior.y) || (P.x == superior.x && P.y == inferior.y)) {
+            return location_t::BORDER; 
+        }
+      }else{
+         return location_t::OUTSIDE;
+      }
+    }
+      // TODO: Substitua o retorno conforme desejar. Isso é apenas um STUB, usado apenas para compilar.
+   return location_t::INVALID;
 }
