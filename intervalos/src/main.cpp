69ce5ec99f286f8c3a80fe4c42d41e6dba50d818
Ronaldo Silva <60142058+ronaldosilva19@users.noreply.github.com>
2022-10-03 16:05:29 -0300

    Update main.cpp

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -15,7 +15,43 @@ using std::setprecision;
 
 int main(void)
 {
-    // TODO: Adicione aqui a sua solução.
+    int x;
+    double Intervalo_0_24 = 0;
+    double Intervalo_25_49 = 0;
+    double Intervalo_50_74 = 0;
+    double Intervalo_75_99 = 0;
+    double ForaIntervalo = 0;
+    
+        while(cin >> std::ws >> x) {
+            if(x >= 0 && x < 25){
+                Intervalo_0_24++;
+            
+        }
+        else{
+        if(x >= 25 && x < 50){
+                Intervalo_25_49++;
+            
+        }else if(x >= 50 && x < 75){
+                Intervalo_50_74++;
+                
+            }else if(x >= 75 && x < 100){
+                Intervalo_75_99++;
+                
+            }else if(x < 0 || x >= 100){
+                ForaIntervalo++;
+            }
+        }    
+    }
+    
+    double somaQuantdigitada = Intervalo_0_24 + Intervalo_25_49 + 
+    Intervalo_50_74 + Intervalo_75_99 + ForaIntervalo;
+    
+    cout<<setprecision(4)<<((Intervalo_0_24 * 100) / somaQuantdigitada)<<endl;
+    cout<<setprecision(4)<<((Intervalo_25_49 * 100) / somaQuantdigitada)<<endl;
+    cout<<setprecision(4)<<((Intervalo_50_74 * 100) / somaQuantdigitada)<<endl;
+    cout<<setprecision(4)<<((Intervalo_75_99 * 100) / somaQuantdigitada)<<endl;
+    cout<<setprecision(4)<<((ForaIntervalo * 100) / somaQuantdigitada)<<endl;
+
 
     return 0;
 }
