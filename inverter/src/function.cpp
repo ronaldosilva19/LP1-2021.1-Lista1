e25b331218e19720f118be9f518fdc0acd267a34
Ronaldo Silva <60142058+ronaldosilva19@users.noreply.github.com>
2022-10-03 16:06:22 -0300

    Update function.cpp

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -8,5 +8,10 @@
 template <std::size_t SIZE>
 void reverse( std::array< std::string, SIZE > & arr )
 {
-    /*codigo*/
+   int tam = arr.size(); // Descobre o tamanho do vetor de strings.
+
+  // Laço que inverte os valores das posiçoes do vetor.
+    for(int i = 0; i < tam / 2; i++){
+      std::swap(arr[i], arr[(tam - 1) - i]);
+    }
 }
